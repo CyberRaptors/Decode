0122b1830326503a43b8a9d7a5a5720abd1ea2c1
Carl Furtado <carlzfurtado@gmail.com>
2025-09-21 12:02:07 -0400

    fix limelight only test opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/normal/opmodes/LimelightOnly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/normal/opmodes/LimelightOnly.java
@@ -1,4 +1,15 @@
 package org.firstinspires.ftc.teamcode.teleop.normal.opmodes;
 
-public class LimelightOnly {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.teleop.normal.runners.LimelightOnlyRunner;
+
+@TeleOp(name="LimelightOnly", group="Linear Opmode")
+public class LimelightOnly extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        new LimelightOnlyRunner().run(this);
+    }
+
 }
