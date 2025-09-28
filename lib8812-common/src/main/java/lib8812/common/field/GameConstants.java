40723b3d30fe49db4b81854815b9c1cfef73a38d
Carl Furtado <carlzfurtado@gmail.com>
2025-09-28 11:41:23 -0400

    add motif AprilTag id constants

--- lib8812-common/src/main/java/lib8812/common/field/GameConstants.java
+++ lib8812-common/src/main/java/lib8812/common/field/GameConstants.java
@@ -2,9 +2,19 @@ package lib8812.common.field;
 
 public class GameConstants {
 	public static class DECODE {
-		public static final int RED_GOAL_APRILTAG_ID = 0;
-		public static final int BLUE_GOAL_APRILTAG_ID = 0;
-		public static final int MOTIF_APRILTAG_ID = 0;
+		public enum Motif
+		{
+			GPP,
+			PGP,
+			PPG
+		}
+
+		public static final int RED_GOAL_APRILTAG_ID = 24;
+		public static final int BLUE_GOAL_APRILTAG_ID = 20;
+
+		public static final int MOTIF_GPP_APRILTAG_ID = 21;
+		public static final int MOTIF_PGP_APRILTAG_ID = 22;
+		public static final int MOTIF_PPG_APRILTAG_ID = 23;
 
 		public static int GOAL_APRILTAG_ID(String alliance) {
 			if (alliance.equals("blue")) return GOAL_APRILTAG_ID(true);
@@ -17,6 +27,12 @@ public class GameConstants {
 			if (blue) return BLUE_GOAL_APRILTAG_ID;
 
 			return RED_GOAL_APRILTAG_ID;
+		}
+
+		public static Motif getMotifFromAprilTagId(int id) {
+			if (id > 23 || id < 21) throw new IllegalArgumentException("AprilTag id does not represent a motif!");
+
+			return Motif.values()[id - 21];
 		}
 	}
 }
