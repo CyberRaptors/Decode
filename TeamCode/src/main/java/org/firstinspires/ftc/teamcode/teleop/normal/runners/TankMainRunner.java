51ac5360597d773dc89bc0b666955b9cbafda917
SilenceRK <rishabhkannan100@gmail.com>
2025-10-10 19:42:11 -0400

    fix TankRaptor reversed motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/normal/runners/TankMainRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/normal/runners/TankMainRunner.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.teleop.normal.runners;
 
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.firstinspires.ftc.teamcode.robot.TankRaptor;
 
 import lib8812.common.robot.IRobot;
@@ -11,11 +14,18 @@ public class TankMainRunner extends ITeleOpRunner {
 	@Override
 	protected void internalRun() {
 		while (opModeIsActive()) {
+			double leftPower = gamepad1.inner.left_stick_y;
+			double rightPower = -gamepad1.inner.right_stick_y;
+
 			bot.leftFront.setPower(gamepad1.inner.left_stick_y);
-			bot.rightFront.setPower(gamepad1.inner.right_stick_y);
+			bot.rightFront.setPower(-gamepad1.inner.right_stick_y);
+
+			telemetry.addData("Left Motor Power", leftPower);
+			telemetry.addData("Right Motor Power", rightPower);
+			telemetry.update();
+
 		}
 	}
-
 	@Override
 	protected IRobot getBot() {
 		return bot;
